e0cbb33275af67d9f4e9757d08fb92ba65a3c56e
SalmanMKC <13schishti@Gmail.com>
2019-05-19 00:15:08 +0100

    Revert "Not complete"
    This reverts commit df8eddb0487eded435c5903cbd48288cd9f6adda.

--- ConsoleApp7/SuffixRotation.cs
+++ ConsoleApp7/SuffixRotation.cs
@@ -1,95 +1,12 @@
-﻿using System.CodeDom.Compiler;
+﻿using System;
 using System.Collections.Generic;
-using System.Collections;
-using System.ComponentModel;
-using System.Diagnostics.CodeAnalysis;
-using System.Globalization;
-using System.IO;
 using System.Linq;
-using System.Reflection;
-using System.Runtime.Serialization;
-using System.Text.RegularExpressions;
 using System.Text;
-using System;
+using System.Threading.Tasks;
 
-class SuffixRotation
+namespace ConsoleApp7
 {
-
-
-
-    static void Main(string[] args)
+    class SuffixRotation
     {
-        int q = Convert.ToInt32(Console.ReadLine());
-
-        for (int qItr = 0; qItr < q; qItr++)
-        {
-            string s = Console.ReadLine();
-            Console.WriteLine(Move(s));
-
-        }
-    }
-
-    private static int Move(string s)
-    {
-        bool ordered = false;
-        int count = 0;
-        while (ordered == false)
-        {
-            StringBuilder first = new StringBuilder();
-            for (int strItr = 0; strItr < s.Length - 1; strItr++)
-            {
-
-                first.Append(s[strItr + 1]);
-            }
-            first.Append(s[0]);
-            string firstcompilled = first.ToString();
-            //Console.WriteLine(firstcompilled);
-            StringBuilder second = new StringBuilder();
-            for (int strItr = 0; strItr < s.Length - 1; strItr++)
-            {
-
-                second.Append(firstcompilled[strItr + 1]);
-            }
-            second.Append(firstcompilled[0]);
-            string secondcompilled = second.ToString();
-            //Console.WriteLine(secondcompilled);
-            StringBuilder third = new StringBuilder();
-            for (int strItr = 0; strItr < s.Length - 1; strItr++)
-            {
-
-                third.Append(secondcompilled[strItr + 1]);
-            }
-            third.Append(secondcompilled[0]);
-            string OneMove = third.ToString();
-            //Console.WriteLine(OneMove);
-
-            char prev = OneMove[0];
-            bool allordered = false;
-            foreach (char c in OneMove)
-            {
-                if (c < prev)
-                {
-                    allordered = false;
-                    break;
-                }
-                else
-                {
-                    allordered = true;
-                    prev = c;
-                }
-            }
-            if (allordered == false)
-            {
-                count++;
-                Move(OneMove);
-            }
-            else
-            {
-                ordered = true;
-                break;
-            }
-        }
-        return count;
-
     }
 }
